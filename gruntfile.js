dd92be4b848a101058254f6cbb65d3571b129a33
Mitchel Jager <mitchel_jager@hotmail.com>
2017-02-15 16:57:28 +0100

    added grunt autoprefixer

--- gruntfile.js
+++ gruntfile.js
@@ -8,6 +8,17 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+		autoprefixer: {
+			options: {
+	      browsers: ['last 10 versions', 'ie 8', 'ie 9', '> 1%']
+	    },
+	    main: {
+        expand: true,
+        flatten: true,
+        src: 'public/css/*.css',
+        dest: 'public/css/'
+	    }
+	  },
 		watch: {
 			css: {
 				files: '**/*.scss',
@@ -17,5 +28,6 @@ module.exports = function(grunt) {
 	});
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.registerTask('default',['watch']);
 };
