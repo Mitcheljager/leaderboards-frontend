c45741146d00485fca31c2f4bdf034cd81f7d12c
Mitchel Jager <mitchel_jager@hotmail.com>
2017-02-24 16:14:00 +0100

    overall design changes to be less generic

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,29 @@
+var menuIsActive = false;
+
 $(function() {
   $('.js-toggle-bubble').on('click', toggleBubble);
+  $('.js-toggle-menu').on('click', toggleMenu);
 });
-
 
 var toggleBubble = function() {
   var target = $(this).next('.js-bubble');
 
   $('.leaderboards-item__bubble--is-active').not(target).removeClass('leaderboards-item__bubble--is-active');
   target.toggleClass('leaderboards-item__bubble--is-active');
+}
+
+var toggleMenu = function() {
+  menuIsActive = !menuIsActive;
+
+  if (menuIsActive) {
+    $('.menu').addClass('menu--is-active');
+    $(this).addClass('menu-trigger--is-active');
+  } else {
+    $('.menu').addClass('menu--is-moving-out');
+    $(this).removeClass('menu-trigger--is-active');
+
+    setTimeout(function() {
+      $('.menu').removeClass('menu--is-moving-out menu--is-active');
+    }, 500);
+  }
 }
