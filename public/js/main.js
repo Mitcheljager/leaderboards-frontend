1ace8856b2ad8101e82a50d14a4d6a07d5531ea3
Mitchel Jager <mitchel_jager@hotmail.com>
2017-03-03 02:18:35 +0100

    Added filter and associated style

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,9 @@
-var menuIsActive = false;
+var menuIsActive = false, currentFilter = 'points';
 
 $(function() {
   $('.js-toggle-bubble').on('click', toggleBubble);
   $('.js-toggle-menu').on('click', toggleMenu);
+  $('.js-filter-item').on('click', toggleFilterOption);
 });
 
 var toggleBubble = function() {
@@ -25,5 +26,27 @@ var toggleMenu = function() {
     setTimeout(function() {
       $('.menu').removeClass('menu--is-moving-out menu--is-active');
     }, 500);
+  }
+}
+
+var toggleFilterOption = function() {
+  var leaderboards = $('.js-leaderboards'),
+      target = $(this).data('target'),
+      itemToMoveOut = 'leaderboards__list--is-moving-out-' + currentFilter;
+
+  if (target != currentFilter) {
+    console.log('leaderboards__list--is-filtered-by-' + currentFilter);
+
+    $('.filter__item--is-active').removeClass('filter__item--is-active');
+    $(this).addClass('filter__item--is-active');
+
+    leaderboards.addClass(itemToMoveOut + ' leaderboards__list--is-filtered-by-' + target);
+    leaderboards.removeClass('leaderboards__list--is-filtered-by-' + currentFilter);
+
+    setTimeout(function() {
+      leaderboards.removeClass(itemToMoveOut);
+    }, 300);
+
+    currentFilter = target;
   }
 }
